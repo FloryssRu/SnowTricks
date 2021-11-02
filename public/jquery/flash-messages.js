b7eaf66503041bd954a9b6122169a739fb5884d6
Floryss Rubechi <floryss.rubechi@gmail.com>
2021-11-02 16:03:56 +0100

    Animation and position of flash messages remake

--- public/jquery/flash-messages.js
+++ public/jquery/flash-messages.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 
-    $("#close").click(function(){
-    	$("#flash").animate({opacity: '0', height: '0'}, 400);
-    });
+    var flash_messages_number = $(".flash-container div.flash").length;
 
+    for (let i = 0; i < flash_messages_number; i++) {
+
+        $("#" + i + "_close").click(function(){
+            $("#" + i + "_flash").animate({opacity: '0', height: '0', margin: '0px', padding: '0px'}, 400);
+        });
+
+    }
 });
